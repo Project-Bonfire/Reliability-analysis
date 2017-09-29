0ab7b43b3f15965aa02af366bf1e5b58ef811194
Thilo Kogge <thilo@kogge.net>
2017-09-29 15:26:14 +0300

    Results are generated now to folders

--- run_sim.sh
+++ run_sim.sh
@@ -16,14 +16,22 @@ source /eda/mentor/2015-16/scripts/QUESTA-SV-AFV_10.4c-5_RHELx86.csh
 
 
 cd simulation
-
+set i=0
+set curtime=`date +%Y-%m-%d.%H:%M:%S`
+mkdir "../results/$curtime"
 foreach line ( "`cat ../sim_runs`" )
         set args=`echo $line:q | sed 's/,/ /g'`
         echo "Current run: $args"
+	i = $i + 1
 	setenv BREAK_TIME_BEFORE "$args[1]"
 	setenv BREAK_TIME_AFTER "$args[2]"
 	setenv FAULT_VALUE "$args[3]"
 	setenv FAULT_LENGTH "$args[4]"
 	setenv BREAK_NAME "$args[5] $args[6]"
 	vsim -c -do simulate.do
+	set respath="../results/$curtime/$i"
+	mkdir "$respath"
+	echo "$line" > "$respath/params.txt"
+	mv ./received.txt "$respath/received.txt"
+	mv ./sent.txt "$respath/sent.txt"
 end
