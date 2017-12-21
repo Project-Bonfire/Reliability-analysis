1e5afe9a08db30b77da2c8ad6f14528f974a46be
Thilo Kogge <thilo@kogge.net>
2017-12-21 14:15:47 +0100

    prepare sim recognizes modules now!

--- prepare_sim/sim_run_creator/sim_run_creator.py
+++ prepare_sim/sim_run_creator/sim_run_creator.py
@@ -1,18 +1,45 @@
 #!/usr/bin/python
 
 import sys
+import re
+import ast
 
 # usage: python sim_run_creator.py num_sims [sim_length_in_ns fault_length]
 from random import randrange
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Creates a fault paramter set for the simulation. Outputs one line for each parameter set.\nSimulation length is earliest-fault-time+latest-fault-time+cooldown-time.')
+parser.add_argument('numexperiments', type=int, help='The number of parameter sets to be generated')
+#parser.add_argument('--verbose',action='store_true',
+#                    help='Prints progress and additional information.')
+parser.add_argument('--latest-fault-time',nargs='?', type=int,
+                    default='10000',help='The latest fault injection time in nanoseconds.')
+parser.add_argument('--earliest-fault-time',nargs='?', type=int,
+                    default='1000',help='The earliest fault injection time in nanoseconds.')
+parser.add_argument('--cooldown-time',nargs='?', type=int,
+                    default='1000',help='How long after the last fault injection should the simulation keep running.')
+parser.add_argument('--length-of-fault',nargs='?', type=int,
+                    default='10',help='How long should the injected fault last in nanoseconds')
+parser.add_argument('--use-module-only',nargs='?', choices=['lbdr','fifo','arbiter','xbar','all'],
+                    default='all',help='Choose only fault locations from the given module. "all" for all modules.')
+parser.add_argument('--no-nofault-line',action='store_false',
+                    help='Does not add a nofault line to the beginning of the output.')
+args = parser.parse_args()
+
+
+
 lines = [line.strip() for line in open('../cell_export_parser/results.txt')]
+module=args.use_module_only
+if module != 'all':
+    lines = [l for l in lines if l.split(' ')[2].strip()[1:] == module]
 
-sim_length = int(sys.argv[2]) if len(sys.argv) > 2 else 10000
-offset = 1000
-fault_length_arg = int(sys.argv[3]) if len(sys.argv) > 3 else 10
-cooldown_time = 1000
+sim_length = args.latest_fault_time
+offset = args.earliest_fault_time
+fault_length_arg = args.length_of_fault
+cooldown_time = args.cooldown_time
 
-for n in range(int(sys.argv[1])):
+for n in range(args.numexperiments):
     break_time_before = randrange(sim_length-offset) + offset
     break_time_after = sim_length - break_time_before + cooldown_time
     fault_value = randrange(2)
